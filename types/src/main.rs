93431b02c3c546a5e9e21991e370c2d408425d91
adam <adam@adams-macbook-pro.local>
2017-09-05 17:09:12 +0100

    add first part of types examples

---
+++ types/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    //Rust comes with the usual built in types you would expect
+    let a: bool = true; //such as booleans
+    let b: usize = 0; //usize type for indices and sizes of containers
+    let c: u32 = 42; //unsigned 32 bit integers
+    let d: u64 = 42; //unsigned 64 bit integers
+    let e: i32 = -458; //signed 32 bit integers
+    let f: i64 = -307883; //signed 64 bit integers
+    let g: f32 = 3.14159; //32 bit floating point numbers
+    let h: f64 = 1.6; //64 bit floating point numbers
+    let i: char = '∯'; //unicode character
+
+    println!("a={:?}", a);
+    println!("b={:?}", b);
+    println!("c={:?}", c);
+    println!("d={:?}", d);
+    println!("e={:?}", e);
+    println!("f={:?}", f);
+    println!("g={:?}", g);
+    println!("h={:?}", h);
+    println!("i={:?}", i);
+
+}
---
+++ types/target/debug/types.d
@@ -0,0 +1,1 @@
+/Users/adam/a_very_short_intro_to_rust/types/target/debug/types: /Users/adam/a_very_short_intro_to_rust/types/src/main.rs
